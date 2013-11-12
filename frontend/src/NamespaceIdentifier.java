ffb9110d62863d6349f3bff167088fb5fe200d62
Lucas Wojciechowski <lucas@lucaswoj.com>
2013-11-12 10:34:50 -0500

    Made everything actually... ahem... compile... again

--- frontend/src/NamespaceIdentifier.java
+++ frontend/src/NamespaceIdentifier.java
@@ -1,13 +1,13 @@
 package org.whdl.frontend;
 
-import java.util.ArrayList;
+import java.util.List;
 
 class NamespaceIdentifier {
 
   private List<String> name;
 
   public NamespaceIdentifier(List<String> name) {
-    this.name = ava.util.Collections.unmodifiableList(name);
+    this.name = java.util.Collections.unmodifiableList(name);
   }
 
   public List<String> getName() {
