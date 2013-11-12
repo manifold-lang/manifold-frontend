286efebfaeb4e709967799ef2c509e064a4173eb
Lucas Wojciechowski <lucas@lucaswoj.com>
2013-11-12 17:26:55 -0500

    Spelling fix

--- src/main/java/org/whdl/frontend/TypeValue.java
+++ src/main/java/org/whdl/frontend/TypeValue.java
@@ -2,13 +2,13 @@ package org.whdl.frontend;
 
 public abstract class TypeValue extends Value {
 
-  /*
+ /*
   * Java doesn't let us express this but every `TypeValue`, by convention,
-  * ought to have a `public abstract final` `instance` property
+  * ought to have a `public abstract final` singleton `instance` property
   *
   *   public abstract final TypeValue instance;
   *
-  * and a singnle private constructor
+  * and a private constructor
   *
   *   private abstract TypeValue();
   */
