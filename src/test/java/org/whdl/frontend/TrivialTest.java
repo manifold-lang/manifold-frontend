c3bf488f28503477986648ac1e1c0b499f9d41d3
Lucas Wojciechowski <lucas@lucaswoj.com>
2013-11-12 10:00:57 -0500

    Initial gradle setup

---
+++ src/test/java/org/whdl/frontend/TrivialTest.java
@@ -0,0 +1,11 @@
+package org.whdl.frontend;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TrivialTest {
+  @Test
+  public void trivial() {
+    assertEquals(true, true);
+  }
+}
