494ff7ae5139d8a7d7212b412fb49bdaf7452cad
Lucas Wojciechowski <lucas@lucaswoj.com>
2013-11-12 19:35:41 -0500

    Added unit tests

---
+++ src/test/java/org/whdl/frontend/VariableIdentifierTest.java
@@ -0,0 +1,37 @@
+package org.whdl.frontend;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.lang.UnsupportedOperationException;
+import java.util.ArrayList;
+
+public class VariableIdentifierTest {
+
+  private NamespaceIdentifier namespaceIdentifierInstance;
+
+  private NamespaceIdentifier getNamespaceIdentifierInstance() {
+    if (namespaceIdentifierInstance == null) {
+      ArrayList<String> name = new ArrayList<String>(1);
+      name.add("whdl");
+
+      namespaceIdentifierInstance = new NamespaceIdentifier(name);
+    }
+
+    return namespaceIdentifierInstance;
+  }
+
+  private VariableIdentifier getInstance() {
+    return new VariableIdentifier(getNamespaceIdentifierInstance(), "foo");
+  }
+
+  @Test
+  public void testGetName() {
+    assertEquals(getInstance().getName(), "foo");
+  }
+
+  @Test
+  public void testGetNamespaceIdentifier() {
+    assertSame(getInstance().getNamespaceIdentifier(),
+               getNamespaceIdentifierInstance());
+  }
+}
