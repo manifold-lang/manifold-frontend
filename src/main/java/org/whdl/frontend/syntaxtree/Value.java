eb2b8fe7e92dfb076feeb322e2f93d6e75bfecb5
Lucas Wojciechowski <lucas@lucaswoj.com>
2013-11-13 15:10:43 -0500

    Added a method for the verification pass on Value

--- src/main/java/org/whdl/frontend/syntaxtree/Value.java
+++ src/main/java/org/whdl/frontend/syntaxtree/Value.java
@@ -3,5 +3,11 @@ package org.whdl.frontend.syntaxtree;
 public abstract class Value {
 
   public abstract TypeValue getTypeValue();
+  
+  /*
+   * Executed during formal verification pass. Any errors should result in an
+   * exception.
+   */
+  public void verify() {}
 
 }
