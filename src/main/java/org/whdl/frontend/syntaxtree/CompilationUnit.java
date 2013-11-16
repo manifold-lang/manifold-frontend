2cd51b9394da061e6e1a4320d5a0e84d0fd45430
Murphy Berzish <murphy.berzish@gmail.com>
2013-11-16 18:20:32 -0500

    change List to Set in CompilationUnit

--- src/main/java/org/whdl/frontend/syntaxtree/CompilationUnit.java
+++ src/main/java/org/whdl/frontend/syntaxtree/CompilationUnit.java
@@ -1,14 +1,15 @@
 package org.whdl.frontend.syntaxtree;
 
 import java.util.List;
+import java.util.Set;
 
 public class CompilationUnit {
 	private NamespaceIdentifier namespace;
 	private List<NamespaceIdentifier> imports;
-	private List<Statement> statements;
+	private Set<Statement> statements;
 	private Scope fileScope;
 	
-	public CompilationUnit(NamespaceIdentifier namespace, List<NamespaceIdentifier> imports, List<Statement> statements){
+	public CompilationUnit(NamespaceIdentifier namespace, List<NamespaceIdentifier> imports, Set<Statement> statements){
 		this.namespace = namespace;
 		this.imports = imports;
 		this.statements = statements;
@@ -17,7 +18,7 @@ public class CompilationUnit {
 	
 	public NamespaceIdentifier getNamespace(){return namespace;}
 	public List<NamespaceIdentifier> getImports(){return imports;}
-	public List<Statement> getStatements(){return statements;}
+	public Set<Statement> getStatements(){return statements;}
 	
 	public void populateScope(Scope s){
 		// FIXME implement this
