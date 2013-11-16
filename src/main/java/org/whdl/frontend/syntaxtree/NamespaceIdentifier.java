e8e39d502adcc8c0fcdd15bf0a5a813b52f9f26c
Murphy Berzish <murphy.berzish@gmail.com>
2013-11-16 17:19:35 -0500

    add hashCode() and equals() to NamespaceIdentifier

--- src/main/java/org/whdl/frontend/syntaxtree/NamespaceIdentifier.java
+++ src/main/java/org/whdl/frontend/syntaxtree/NamespaceIdentifier.java
@@ -29,4 +29,30 @@ public class NamespaceIdentifier {
 	  }
 	  return sb.toString();
   }
+  
+  @Override
+  public int hashCode(){
+	  int hash = 1;
+	  for(String n : name){
+		  hash = hash * 17 + n.hashCode();
+	  }
+	  return hash;
+  }
+  
+  @Override
+  public boolean equals(Object aThat){
+	  if(this == aThat) return true;
+	  if(!(aThat instanceof NamespaceIdentifier)) return false;
+	  NamespaceIdentifier that = (NamespaceIdentifier)aThat;
+	  List<String> thisName = this.getName();
+	  List<String> thatName = that.getName();
+	  // two namespace identifiers are equal if they name the same namespace
+	  if(thisName.size() != thatName.size()) return false;
+	  for(int i = 0; i < thisName.size(); ++i){
+		  String nThis = thisName.get(i);
+		  String nThat = thatName.get(i);
+		  if(!nThis.equals(nThat)) return false;
+	  }
+	  return true;
+  }
 }
