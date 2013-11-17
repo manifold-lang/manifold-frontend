7593c3bf6579f2186bdf2cc3de8194d138e048b9
Murphy Berzish <murphy.berzish@gmail.com>
2013-11-16 20:58:24 -0500

    reformatting of Variable, no syntactic changes

--- src/main/java/org/whdl/frontend/syntaxtree/Variable.java
+++ src/main/java/org/whdl/frontend/syntaxtree/Variable.java
@@ -3,38 +3,48 @@ package org.whdl.frontend.syntaxtree;
 public class Variable {
 	private VariableIdentifier identifier;
 	private Expression typeExpression;
-	
+
 	private boolean assigned = false;
 	private Expression valExpr;
 
-	public Variable(VariableIdentifier identifier, Expression typeExpression){
+	public Variable(VariableIdentifier identifier, Expression typeExpression) {
 		this.identifier = identifier;
 		this.typeExpression = typeExpression;
 	}
-	public VariableIdentifier getIdentifier(){return identifier;}
-	public TypeValue getTypeValue() throws TypeMismatchException{
-		// FIXME if it is possible to type-check this expression earlier, it should be done as soon as possible
+
+	public VariableIdentifier getIdentifier() {
+		return identifier;
+	}
+
+	public TypeValue getTypeValue() throws TypeMismatchException {
+		// FIXME if it is possible to type-check this expression earlier, it
+		// should be done as soon as possible
 		// e.g. in a semantic analysis pass
 		Value val = typeExpression.evaluate();
-		if(!(val instanceof TypeValue)){
-			throw new TypeMismatchException(TypeTypeValue.getInstance(), val.getType());
+		if (!(val instanceof TypeValue)) {
+			throw new TypeMismatchException(TypeTypeValue.getInstance(),
+					val.getType());
 		}
-		return (TypeValue)val;
+		return (TypeValue) val;
 	}
-	
-	public boolean isAssigned(){return assigned;}
-	public Value getValue() throws VariableNotAssignedException{
-		if(!isAssigned()){
+
+	public boolean isAssigned() {
+		return assigned;
+	}
+
+	public Value getValue() throws VariableNotAssignedException {
+		if (!isAssigned()) {
 			throw new VariableNotAssignedException(this);
 		}
 		return valExpr.evaluate();
 	}
-	public void setValue(Expression valExpr) throws MultipleAssignmentException{
-		if(isAssigned()){
+
+	public void setValue(Expression valExpr) throws MultipleAssignmentException {
+		if (isAssigned()) {
 			throw new MultipleAssignmentException(this);
 		}
 		this.valExpr = valExpr;
 		this.assigned = true;
 	}
-	
+
 }
