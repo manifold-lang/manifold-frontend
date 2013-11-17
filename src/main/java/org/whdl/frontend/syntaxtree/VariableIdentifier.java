5277e3b6d1a42bc0c469948795bbcd83dee9cca1
Murphy Berzish <murphy.berzish@gmail.com>
2013-11-16 21:10:47 -0500

    add equals() to VariableIdentifier

--- src/main/java/org/whdl/frontend/syntaxtree/VariableIdentifier.java
+++ src/main/java/org/whdl/frontend/syntaxtree/VariableIdentifier.java
@@ -2,31 +2,43 @@ package org.whdl.frontend.syntaxtree;
 
 public class VariableIdentifier {
 
-  private String name;
-  private NamespaceIdentifier namespaceIdentifier;
+	private String name;
+	private NamespaceIdentifier namespaceIdentifier;
 
-  public VariableIdentifier(NamespaceIdentifier namespaceIdentifier,
-                            String name) {
-    this.name = name;
-    this.namespaceIdentifier = namespaceIdentifier;
-  }
+	public VariableIdentifier(NamespaceIdentifier namespaceIdentifier,
+			String name) {
+		this.name = name;
+		this.namespaceIdentifier = namespaceIdentifier;
+	}
 
-  public String getName() {
-    return name;
-  }
-  
-  public NamespaceIdentifier getNamespaceIdentifier() {
-    return namespaceIdentifier;
-  }
-  
-  @Override
-  public String toString(){
-	  String nsString = getNamespaceIdentifier().toString();
-	  if(nsString.isEmpty()){
-		  return getName();
-	  } else {
-		  // FIXME this is syntax-dependent
-		  return nsString + "." + getName();
-	  }
-  }
+	public String getName() {
+		return name;
+	}
+
+	public NamespaceIdentifier getNamespaceIdentifier() {
+		return namespaceIdentifier;
+	}
+
+	@Override
+	public String toString() {
+		String nsString = getNamespaceIdentifier().toString();
+		if (nsString.isEmpty()) {
+			return getName();
+		} else {
+			// FIXME this is syntax-dependent
+			return nsString + "." + getName();
+		}
+	}
+
+	@Override
+	public boolean equals(Object aThat) {
+		if (this == aThat)
+			return true;
+		if (!(aThat instanceof VariableIdentifier))
+			return false;
+		VariableIdentifier that = (VariableIdentifier) aThat;
+		// two variable identifiers are equal if they have the same namespace and name
+		return (this.getName().equals(that.getName()) && 
+				this.getNamespaceIdentifier().equals(that.getNamespaceIdentifier()));
+	}
 }
