45c63d5aaa61ff3d06de9395686fe805aa031813
Murphy Berzish <murphy.berzish@gmail.com>
2014-04-01 20:07:28 -0400

    add "from" and "to" members on Connection, and parent-Node member on Endpoint

--- src/main/java/org/whdl/intermediate/Connection.java
+++ src/main/java/org/whdl/intermediate/Connection.java
@@ -7,11 +7,13 @@ public class Connection {
   private String instanceName;
   private String typename;
   private Map<String, Expression> attributes;
-  // FIXME reference to "from" and "to" connection point
+  private Endpoint eFrom, eTo;
   
-  public Connection(String instanceName, String typename){
+  public Connection(String instanceName, String typename, Endpoint eFrom, Endpoint eTo){
     this.instanceName = instanceName;
     this.typename = typename;
     this.attributes = new HashMap<String, Expression>();
+    this.eFrom = eFrom;
+    this.eTo = eTo;
   }
 }
--- src/main/java/org/whdl/intermediate/Endpoint.java
+++ src/main/java/org/whdl/intermediate/Endpoint.java
@@ -7,10 +7,12 @@ public class Endpoint {
   private String instanceName;
   private String typename;
   private Map<String, Expression> attributes;
+  private Node parent;
   
-  public Endpoint(String instanceName, String typename){
+  public Endpoint(String instanceName, String typename, Node parent){
     this.instanceName = instanceName;
     this.typename = typename;
     this.attributes = new HashMap<String, Expression>();
+    this.parent = parent;
   }
 }
