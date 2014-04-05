13563109baad1bea62f87b1ceefc29585fc34458
Murphy Berzish <murphy.berzish@gmail.com>
2014-04-05 18:14:12 -0400

    type classes for domain objects

---
+++ src/main/java/org/whdl/intermediate/types/ConnectionType.java
@@ -0,0 +1,42 @@
+package org.whdl.intermediate.types;
+
+import org.whdl.intermediate.Type;
+
+public class ConnectionType implements Type {
+  private String definitionName;
+  
+  public ConnectionType(String definitionName){
+    this.definitionName = definitionName;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result
+        + ((definitionName == null) ? 0 : definitionName.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    ConnectionType other = (ConnectionType) obj;
+    if (definitionName == null) {
+      if (other.definitionName != null) {
+        return false;
+      }
+    } else if (!definitionName.equals(other.definitionName)) {
+      return false;
+    }
+    return true;
+  }
+}
---
+++ src/main/java/org/whdl/intermediate/types/EndpointType.java
@@ -0,0 +1,42 @@
+package org.whdl.intermediate.types;
+
+import org.whdl.intermediate.Type;
+
+public class EndpointType implements Type {
+  private String definitionName;
+  
+  public EndpointType(String definitionName){
+    this.definitionName = definitionName;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result
+        + ((definitionName == null) ? 0 : definitionName.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    EndpointType other = (EndpointType) obj;
+    if (definitionName == null) {
+      if (other.definitionName != null) {
+        return false;
+      }
+    } else if (!definitionName.equals(other.definitionName)) {
+      return false;
+    }
+    return true;
+  }
+}
