0bb6ea72ae66b276ebbc5092cb19be40f4c1b680
Murphy Berzish <murphy.berzish@gmail.com>
2014-04-05 17:19:36 -0400

    constraints are no longer associated with expressions
    (this means they are treated like black boxes until the backend,
    similar to nodes/connections/etc. are now)

--- src/main/java/org/whdl/intermediate/Constraint.java
+++ src/main/java/org/whdl/intermediate/Constraint.java
@@ -6,13 +6,11 @@ import java.util.Map;
 public class Constraint {
   private String instanceName;
   private String typename;
-  private Map<String, Expression> arguments;
-  private Expression constraintExpr;
+  private Map<String, Expression> arguments;  
   
-  public Constraint(String instanceName, String typename, Expression expr){
+  public Constraint(String instanceName, String typename){
     this.instanceName = instanceName;
     this.typename = typename;
     this.arguments = new HashMap<String, Expression>();
-    this.constraintExpr = expr;
   }
 }
--- src/main/java/org/whdl/intermediate/definitions/ConstraintDefinition.java
+++ src/main/java/org/whdl/intermediate/definitions/ConstraintDefinition.java
@@ -8,11 +8,9 @@ import org.whdl.intermediate.Expression;
 
 public class ConstraintDefinition extends Definition {
   private Map<String, TypeDefinition> arguments;
-  private Expression constraintExpr;
   
   public ConstraintDefinition(String typename, Expression constraintExpr){
     super(typename);
     this.arguments = new HashMap<String, TypeDefinition>();
-    this.constraintExpr = constraintExpr; // FIXME how/when to verify correctness of this expr when argument names are specified later?
   }
 }
