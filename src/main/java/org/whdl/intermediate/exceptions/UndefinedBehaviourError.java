73c35014d8e5e625e9919298f346c70f332d9cad
Murphy Berzish <murphy.berzish@gmail.com>
2014-04-06 13:28:03 -0400

    nit: remove empty comment block

--- src/main/java/org/whdl/intermediate/exceptions/UndefinedBehaviourError.java
+++ src/main/java/org/whdl/intermediate/exceptions/UndefinedBehaviourError.java
@@ -2,9 +2,6 @@ package org.whdl.intermediate.exceptions;
 
 public class UndefinedBehaviourError extends Error {
 
-  /**
-   * 
-   */
   private static final long serialVersionUID = 4302832267210981600L;
 
   private String msg;
