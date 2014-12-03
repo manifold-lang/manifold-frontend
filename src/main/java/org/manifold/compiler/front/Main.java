f0fe6fafbfe79498b7aedb167f5c1e94c07ea3d1
Murphy Berzish <murphy.berzish@gmail.com>
2014-12-03 13:12:54 -0500

    remove mocked-up digital circuit primitive definitions
    ...IT BEGINS.

--- src/main/java/org/manifold/compiler/front/Main.java
+++ src/main/java/org/manifold/compiler/front/Main.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -17,18 +16,11 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.manifold.compiler.BooleanTypeValue;
 import org.manifold.compiler.BooleanValue;
-import org.manifold.compiler.ConnectionType;
 import org.manifold.compiler.Frontend;
 import org.manifold.compiler.IntegerValue;
-import org.manifold.compiler.NilTypeValue;
-import org.manifold.compiler.NodeTypeValue;
-import org.manifold.compiler.PortTypeValue;
 import org.manifold.compiler.TypeValue;
-import org.manifold.compiler.UndeclaredIdentifierException;
 import org.manifold.compiler.UndefinedBehaviourError;
-import org.manifold.compiler.Value;
 import org.manifold.compiler.middle.Schematic;
 import org.manifold.parser.ManifoldBaseVisitor;
 import org.manifold.parser.ManifoldLexer;
@@ -37,8 +29,6 @@ import org.manifold.parser.ManifoldParser.ExpressionContext;
 import org.manifold.parser.ManifoldParser.NamespacedIdentifierContext;
 import org.manifold.parser.ManifoldParser.TupleTypeValueContext;
 import org.manifold.parser.ManifoldParser.TypevalueContext;
-
-import com.google.common.annotations.VisibleForTesting;
 
 public class Main implements Frontend {
 
@@ -84,7 +74,9 @@ public class Main implements Frontend {
     }
 
     log.debug("expressions:");
-    log.debug(expressions);
+    for (Expression expr : expressions) {
+      log.debug(expr.toString());
+    }
 
     Map<NamespaceIdentifier, Namespace> namespaces = new HashMap<>();
 
@@ -94,10 +86,6 @@ public class Main implements Frontend {
     // "top-level" bindings go into the private scope of the default namespace
 
     Schematic schematic = new Schematic(inputFile.getName());
-
-    // mock-up: digital circuits primitives
-    // (to be removed when core library and namespaces are implemented)
-    createDigitalPrimitives(defaultNamespace.getPrivateScope(), schematic);
 
     // Build top-level scope
     for (Expression expr : expressions) {
@@ -143,7 +131,7 @@ public class Main implements Frontend {
       NamespaceIdentifier name = id.getNamespaceIdentifier();
       Namespace ns = namespaces.get(name);
       Variable v = ns.getPrivateScope().getVariable(id);
-      log.debug(v.getIdentifier() + " ::= " + v.getType());
+      log.debug(v.getIdentifier() + " ::= " + v.getType().toString());
     }
 
     ExpressionGraph exprGraph = new ExpressionGraph(
@@ -165,151 +153,6 @@ public class Main implements Frontend {
 
     return schematic;
   }
-
-  private static void setupDigitalTypes(Schematic s)
-      throws org.manifold.compiler.MultipleDefinitionException,
-      UndeclaredIdentifierException {
-    PortTypeValue digitalInPortType;
-    PortTypeValue digitalOutPortType;
-
-    Map<String, TypeValue> noTypeAttributes = new HashMap<>();
-    Map<String, Value> noAttributes = new HashMap<>();
-
-    Map<String, TypeValue> registerTypeAttributes = new HashMap<>();
-    Map<String, PortTypeValue> registerTypePorts = new HashMap<>();
-    NodeTypeValue registerType;
-
-    Map<String, PortTypeValue> andTypePorts = new HashMap<>();
-    NodeTypeValue andType;
-
-    Map<String, PortTypeValue> orTypePorts = new HashMap<>();
-    NodeTypeValue orType;
-
-    Map<String, PortTypeValue> notTypePorts = new HashMap<>();
-    NodeTypeValue notType;
-
-    Map<String, PortTypeValue> inputPinTypePorts = new HashMap<>();
-    NodeTypeValue inputPinType;
-
-    Map<String, PortTypeValue> outputPinTypePorts = new HashMap<>();
-    NodeTypeValue outputPinType;
-
-    ConnectionType digitalWireType;
-
-    digitalInPortType = new PortTypeValue(s.getUserDefinedType("Bool"),
-        noTypeAttributes);
-    digitalOutPortType = new PortTypeValue(s.getUserDefinedType("Bool"),
-        noTypeAttributes);
-
-    registerTypeAttributes.put("initialValue", BooleanTypeValue.getInstance());
-    registerTypeAttributes.put("resetActiveHigh",
-        BooleanTypeValue.getInstance());
-    registerTypeAttributes.put("resetAsynchronous",
-        BooleanTypeValue.getInstance());
-    registerTypeAttributes.put("clockActiveHigh",
-        BooleanTypeValue.getInstance());
-    registerTypePorts.put("in", digitalInPortType);
-    registerTypePorts.put("out", digitalOutPortType);
-    registerTypePorts.put("clock", digitalInPortType);
-    registerTypePorts.put("reset", digitalInPortType);
-    registerType = new NodeTypeValue(registerTypeAttributes, registerTypePorts);
-
-    andTypePorts.put("in0", digitalInPortType);
-    andTypePorts.put("in1", digitalInPortType);
-    andTypePorts.put("out", digitalOutPortType);
-    andType = new NodeTypeValue(noTypeAttributes, andTypePorts);
-
-    orTypePorts.put("in0", digitalInPortType);
-    orTypePorts.put("in1", digitalInPortType);
-    orTypePorts.put("out", digitalOutPortType);
-    orType = new NodeTypeValue(noTypeAttributes, orTypePorts);
-
-    notTypePorts.put("in", digitalInPortType);
-    notTypePorts.put("out", digitalOutPortType);
-    notType = new NodeTypeValue(noTypeAttributes, notTypePorts);
-
-    inputPinTypePorts.put("out", digitalOutPortType);
-    inputPinType = new NodeTypeValue(noTypeAttributes, inputPinTypePorts);
-
-    outputPinTypePorts.put("in", digitalInPortType);
-    outputPinType = new NodeTypeValue(noTypeAttributes, outputPinTypePorts);
-
-    digitalWireType = new ConnectionType(noTypeAttributes);
-
-    s.addPortType("digitalIn", digitalInPortType);
-    s.addPortType("digitalOut", digitalOutPortType);
-
-    s.addNodeType("register", registerType);
-    s.addNodeType("and", andType);
-    s.addNodeType("or", orType);
-    s.addNodeType("not", notType);
-    s.addNodeType("inputPin", inputPinType);
-    s.addNodeType("outputPin", outputPinType);
-
-    s.addConnectionType("digitalWire", digitalWireType);
-
-  }
-
-  @VisibleForTesting
-  public static void createDigitalPrimitives(Scope scope, Schematic schematic)
-      throws MultipleDefinitionException, VariableNotDefinedException,
-      MultipleAssignmentException, UndeclaredIdentifierException,
-      org.manifold.compiler.MultipleDefinitionException {
-    setupDigitalTypes(schematic);
-    // inputPin: unit -> Bool
-    FunctionTypeValue inputPinPrimitiveType = new FunctionTypeValue(
-        NilTypeValue.getInstance(), BooleanTypeValue.getInstance());
-    PrimitiveFunctionValue inputPinPrimitive = new PrimitiveFunctionValue(
-        "inputPin", inputPinPrimitiveType, schematic.getNodeType("inputPin"));
-    VariableIdentifier inputPinIdentifier = new VariableIdentifier(
-        Arrays.asList(new String[]{"inputPin"}));
-    scope.defineVariable(inputPinIdentifier);
-    scope.assignVariable(inputPinIdentifier,
-        new LiteralExpression(inputPinPrimitive));
-    // outputPin: Bool -> unit
-    FunctionTypeValue outputPinPrimitiveType = new FunctionTypeValue(
-        BooleanTypeValue.getInstance(), NilTypeValue.getInstance());
-    PrimitiveFunctionValue outputPinPrimitive = new PrimitiveFunctionValue(
-        "outputPin", outputPinPrimitiveType,
-        schematic.getNodeType("outputPin"));
-    VariableIdentifier outputPinIdentifier = new VariableIdentifier(
-        Arrays.asList(new String[]{"outputPin"}));
-    scope.defineVariable(outputPinIdentifier);
-    scope.assignVariable(outputPinIdentifier,
-        new LiteralExpression(outputPinPrimitive));
-    // and: (Bool, Bool) -> Bool
-    FunctionTypeValue andPrimitiveType = new FunctionTypeValue(
-        new TupleTypeValue(Arrays.asList(new TypeValue[]{
-            BooleanTypeValue.getInstance(), BooleanTypeValue.getInstance()
-        })), BooleanTypeValue.getInstance());
-    PrimitiveFunctionValue andPrimitive = new PrimitiveFunctionValue(
-        "and", andPrimitiveType, schematic.getNodeType("and"));
-    VariableIdentifier andIdentifier = new VariableIdentifier(
-        Arrays.asList(new String[]{"and"}));
-    scope.defineVariable(andIdentifier);
-    scope.assignVariable(andIdentifier, new LiteralExpression(andPrimitive));
-    // or: (Bool, Bool) -> Bool
-    FunctionTypeValue orPrimitiveType = new FunctionTypeValue(
-        new TupleTypeValue(Arrays.asList(new TypeValue[]{
-            BooleanTypeValue.getInstance(), BooleanTypeValue.getInstance()
-        })), BooleanTypeValue.getInstance());
-    PrimitiveFunctionValue orPrimitive = new PrimitiveFunctionValue(
-        "or", orPrimitiveType, schematic.getNodeType("or"));
-    VariableIdentifier orIdentifier = new VariableIdentifier(
-        Arrays.asList(new String[]{"or"}));
-    scope.defineVariable(orIdentifier);
-    scope.assignVariable(orIdentifier, new LiteralExpression(orPrimitive));
-    // not: Bool -> Bool
-    FunctionTypeValue notPrimitiveType = new FunctionTypeValue(
-        BooleanTypeValue.getInstance(), BooleanTypeValue.getInstance());
-    PrimitiveFunctionValue notPrimitive = new PrimitiveFunctionValue(
-        "not", notPrimitiveType, schematic.getNodeType("not"));
-    VariableIdentifier notIdentifier = new VariableIdentifier(
-        Arrays.asList(new String[]{"not"}));
-    scope.defineVariable(notIdentifier);
-    scope.assignVariable(notIdentifier, new LiteralExpression(notPrimitive));
-  }
-
 }
 
 class ExpressionContextVisitor extends ManifoldBaseVisitor<Expression> {
