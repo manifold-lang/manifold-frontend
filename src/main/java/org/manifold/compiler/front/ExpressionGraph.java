c49effbafec369609c7e2c2aad3c99694dc168a4
maxqchen <max.c255@gmail.com>
2015-02-11 18:14:09 -0500

    #56 Adding subgraph to graph
    Throwing up some code for PR first, will update PR with tests once the function itself is good :)

--- src/main/java/org/manifold/compiler/front/ExpressionGraph.java
+++ src/main/java/org/manifold/compiler/front/ExpressionGraph.java
@@ -96,6 +96,56 @@ public class ExpressionGraph {
   public ExpressionGraph() {
   }
 
+  /**
+   * Adds the provided subgraph to this graph
+   * Precondition: there are no variable id conflicts between subGraph and this graph. That is
+   *               each variable identifier in the subgraph is generated WRT its scope.
+   *
+   * @param subGraph ExpressionGraph to be added (elaborated function)
+   * @param inputEdge edge of this graph to connect to subgraph (function args)
+   * @param inputVertex vertex in the subgraph that inputEdge should connect to (function args)
+   * @param outputVertex vertex in the subgraph that outputEdges should connect from (function return)
+   * @param outputEdges edges that outputVertex connect from (function return)
+   */
+  public void addSubExpressionGraph(ExpressionGraph subGraph,
+                                    ExpressionEdge inputEdge, ExpressionVertex inputVertex,
+                                    VariableReferenceVertex outputVertex, List<ExpressionEdge> outputEdges) {
+
+    // Sanity checks
+    // input/output vertices exist in subgraph
+    Preconditions.checkArgument(subGraph.allVertices.containsAll(
+        ImmutableList.of(inputVertex, outputVertex)));
+
+    // edges exist in main graph
+    // TODO(max) - edge check is kind of expensive (O(n) search through edges) maybe change to a set?
+    Preconditions.checkArgument(this.edges.containsAll(
+        ImmutableList.of(inputEdge, outputEdges)));
+
+    // all output edges have common source
+    Preconditions.checkArgument(outputEdges.stream()
+        .map(ExpressionEdge::getSource)
+        .distinct()
+        .count() == 1);
+
+    // subGraph is being thrown away after, so we can just add the vertices/edges directly
+    subGraph.allVertices.forEach(this::addVertex);
+    subGraph.edges.forEach(this::addEdge);
+
+    // removing old vertices - kind of awkward, but they're guaranteed to be var references (right?)
+    VariableReferenceVertex oldInputVertex = (VariableReferenceVertex) inputEdge.getTarget();
+    VariableReferenceVertex oldOutputVertex = (VariableReferenceVertex) outputEdges.get(0).getSource();
+
+    allVertices.remove(oldInputVertex);
+    allVertices.remove(oldOutputVertex);
+    variableVertices.remove(oldInputVertex.getId());
+    variableVertices.remove(oldOutputVertex.getId());
+
+    // set the edges
+    inputEdge.setTarget(inputVertex);
+    outputEdges.forEach(edge -> edge.setSource(outputVertex));
+  }
+
+
   public void writeDOTFile(File file) throws IOException {
     FileWriter fw = new FileWriter(file);
     try (BufferedWriter writer = new BufferedWriter(fw)) {
